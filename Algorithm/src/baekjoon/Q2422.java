b8f42355a045b95c23a307fb53e8a782bb2f1373
hjm7091 <hjm7091@naver.com>
2019-12-28 13:06:24 +0900

    2422번 완료

--- Algorithm/src/baekjoon/Q2422.java
+++ Algorithm/src/baekjoon/Q2422.java
@@ -3,63 +3,42 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class Q2422 {
 
 	static int N,M;
-	static boolean[] check;
-	static ArrayList<HashSet<Integer>> sets = new ArrayList<HashSet<Integer>>();
-	static ArrayList<Integer> combination = new ArrayList<Integer>();
+	static boolean[][] visit = new boolean[500][500];
 	static int result; 
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		String[] firstLine = bufferedReader.readLine().trim().split(" ");
-		N = Integer.parseInt(firstLine[0]);
-		M = Integer.parseInt(firstLine[1]);
-		check = new boolean[N+1];
+		StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		for(int i=0; i<M; i++) {
-			String[] line = bufferedReader.readLine().trim().split(" ");
-			HashSet<Integer> set = new HashSet<Integer>();
-			for(String s : line) {
-				set.add(Integer.parseInt(s));
-			}
-			sets.add(set);
+			st = new StringTokenizer(bufferedReader.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			visit[a][b] = true;
+			visit[b][a] = true;
 		}
-		makeCombination(1);
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=i+1; j<=N; j++) {
+				for(int k=j+1; k<=N; k++) {
+					if(canEat(i,j,k))
+						result++;
+				}
+			}
+		}
+		
 		System.out.println(result);
 	}
 	
-	static public void makeCombination(int n) {
-		if(combination.size()==3) {
-			HashSet<Integer> nowSet = new HashSet<Integer>();
-			for(int num : combination) {
-				nowSet.add(num);
-			}
-			if(check(nowSet)) {
-				System.out.println(nowSet);
-				result++;
-			}
-			return;
-		}
-		for(int i=n; i<=N; i++) {
-			if(!check[i]) {
-				check[i] = true;
-				combination.add(i);
-				makeCombination(i);
-				combination.remove(combination.size()-1);
-				check[i] = false;
-			}
-		}
-	}
-	
-	static public boolean check(HashSet<Integer> nowSet) {
-		for(HashSet<Integer> set : sets) {
-			if(nowSet.containsAll(set))
-				return false;
-		}
+	static public boolean canEat(int i, int j, int k) {
+		if(visit[i][j] || visit[j][i] || visit[i][k] || visit[k][i] || visit[j][k] || visit[k][j])
+			return false;
 		return true;
 	}
 	
