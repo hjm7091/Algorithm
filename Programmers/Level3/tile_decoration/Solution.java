0d508e37a7e62a56add8b218beb5570c6de08bf1
hjm7091 <hjm7091@naver.com>
2020-01-30 21:25:58 +0900

    프로그래머스 알고리즘 연습
    타일 장식물, 네트워크, 섬 연결하기

---
+++ Programmers/Level3/tile_decoration/Solution.java
@@ -0,0 +1,30 @@
+package tile_decoration;
+
+public class Solution {
+
+	long[] d;
+	
+	public long solution(int N) {
+		init(N);
+		findAnswer(N);
+		return d[N];
+	}
+	
+	public void init(int N) {
+		d = new long[N+1];
+		d[1] = 4;
+		d[2] = 6;
+	}
+	
+	public void findAnswer(int N) {
+		for(int i=3; i<=N; i++) {
+			d[i] = d[i-1] + d[i-2];
+		}
+	}
+	
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.solution(80));
+	}
+
+}
