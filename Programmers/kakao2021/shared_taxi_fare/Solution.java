b512487c6dd570425b60b7acfc2deb8ff2a5e06c
hjm7091 <hjm7091@naver.com>
2021-03-17 22:18:38 +0900

    카카오 2021(합승 택시 요금) 완료

---
+++ Programmers/kakao2021/shared_taxi_fare/Solution.java
@@ -0,0 +1,61 @@
+package shared_taxi_fare;
+
+/*
+ * Floyd Warshall 알고리즘
+ */
+public class Solution {
+	
+	final int INF = 200 * 100000; 
+	
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+
+    	int[][] d = new int[n + 1][n + 1];
+    	
+    	for(int i = 1; i <= n; i++) {
+    		for(int j = 1; j <= n; j++) {
+    			if(i == j) d[i][j] = 0;
+    			else d[i][j] = INF;
+    		}
+    	}
+    	
+    	printArray("init", d);
+    	
+    	for(int[] fare : fares) {
+    		int from = fare[0], to = fare[1], cost = fare[2];
+    		d[from][to] = cost;
+    		d[to][from] = cost;
+    	}
+    	
+    	printArray("before floyd", d);
+    	
+    	for(int k = 1; k <= n; k++) {
+    		for(int i = 1; i <= n; i++) {
+    			for(int j = 1; j <= n; j++) {
+    				d[i][j] = Math.min(d[i][j], d[i][k] + d[k][j]);
+    			}
+    		}
+    		printArray("after step k : " + k, d);
+    	}
+    	
+    	int result = Integer.MAX_VALUE;
+    	
+    	for(int k = 1; k <= n; k++) {
+    		result = Math.min(result, d[s][k] + d[k][a] + d[k][b]);
+    	}
+    	
+    	return result;
+	}
+    
+    private void printArray(String msg, int[][] array) {
+    	System.out.println(msg);
+    	for(int i = 1; i < array.length; i++) {
+    		for(int j = 1; j < array.length; j++) {
+    			if(array[i][j] == INF) System.out.print("-1 ");
+    			else System.out.print(array[i][j] + " ");
+    		}
+    		System.out.println();
+    	}
+    	System.out.println();
+    }
+
+}
---
+++ Programmers/kakao2021/shared_taxi_fare/SolutionTest.java
@@ -0,0 +1,32 @@
+package shared_taxi_fare;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+	Solution s = new Solution();
+	
+	@Test
+	public void test1() {
+		int[][] fares = {{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}};
+		int actual = s.solution(6, 4, 6, 2, fares);
+		assertEquals(82, actual);
+	}
+	
+	@Test
+	public void test2() {
+		int[][] fares = {{5, 7, 9}, {4, 6, 4}, {3, 6, 1}, {3, 2, 3}, {2, 1, 6}};
+		int actual = s.solution(7, 3, 4, 1, fares);
+		assertEquals(14, actual);
+	}
+	
+	@Test
+	public void test3() {
+		int[][] fares = {{2, 6, 6}, {6, 3, 7}, {4, 6, 7}, {6, 5, 11}, {2, 5, 12}, {5, 3, 20}, {2, 4, 8}, {4, 3, 9}};
+		int actual = s.solution(6, 4, 5, 6, fares);
+		assertEquals(18, actual);
+	}
+
+}
