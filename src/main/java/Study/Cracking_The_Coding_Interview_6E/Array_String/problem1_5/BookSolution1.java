867dbc4b8c5763d0b060338d55b12c9364124b35
hjm7091 <hjm7091@naver.com>
2022-01-19 23:16:41 +0900

    문제 1.5 연습

---
+++ src/main/java/Study/Cracking_The_Coding_Interview_6E/Array_String/problem1_5/BookSolution1.java
@@ -0,0 +1,50 @@
+package Study.Cracking_The_Coding_Interview_6E.Array_String.problem1_5;
+
+public class BookSolution1 {
+
+    public static void main(String[] args) {
+        BookSolution1 solution = new BookSolution1();
+        System.out.println(solution.oneEditAway("pale", "ple"));
+        System.out.println(solution.oneEditAway("pales", "pale"));
+        System.out.println(solution.oneEditAway("pale", "bale"));
+        System.out.println(solution.oneEditAway("pale", "bake"));
+    }
+
+    public boolean oneEditAway(String first, String second) {
+        if (first.length() == second.length()) {
+            return oneEditReplace(first, second);
+        } else if (first.length() + 1 == second.length()) {
+            return oneEditInsert(first, second);
+        } else if (first.length() - 1 == second.length()) {
+            return oneEditInsert(second, first);
+        }
+        return false;
+    }
+
+    private boolean oneEditReplace(String s1, String s2) {
+        boolean foundDifference = false;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (foundDifference) return false;
+                foundDifference = true;
+            }
+        }
+        return true;
+    }
+
+    private boolean oneEditInsert(String str1, String str2) {
+        int index1 = 0;
+        int index2 = 0;
+        while (index1 < str1.length() && index2 < str2.length()) {
+            if (str1.charAt(index1) != str2.charAt(index2)) {
+                if (index1 != index2) {
+                    return false;
+                }
+            } else {
+                index1++;
+            }
+            index2++;
+        }
+        return true;
+    }
+}
