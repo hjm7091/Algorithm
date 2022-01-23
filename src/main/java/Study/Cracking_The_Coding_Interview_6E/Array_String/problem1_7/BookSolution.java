0819c6353ce957741a5cc03d3c353803009154ae
hjm7091 <hjm7091@naver.com>
2022-01-23 19:05:24 +0900

    코딩 인터뷰 완전 분석 (문제 1.7 연습)

---
+++ src/main/java/Study/Cracking_The_Coding_Interview_6E/Array_String/problem1_7/BookSolution.java
@@ -0,0 +1,25 @@
+package Study.Cracking_The_Coding_Interview_6E.Array_String.problem1_7;
+
+public class BookSolution {
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        if (n == 0 || matrix.length != matrix[0].length) return;
+        for (int layer = 0; layer < n / 2; layer++) {
+            int last = n - 1 - layer;
+            for (int i = layer; i < last; i++) {
+                int offset = i - layer;
+                int top = matrix[layer][i];
+
+                matrix[layer][i] = matrix[last - offset][layer];
+
+                matrix[last - offset][layer] = matrix[last][last - offset];
+
+                matrix[last][last - offset] = matrix[i][last];
+
+                matrix[i][last] = top;
+            }
+        }
+    }
+
+}
