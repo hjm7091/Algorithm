3b4ceeb773429a4f783ad79848b8d52625ef9705
hjm7091 <hjm7091@naver.com>
2022-02-03 22:58:55 +0900

    카카오 2022 코딩테스트 연습(k진수에서 소수 개수 구하기)

---
+++ src/main/java/Programmers/kakao2022/find_decimals_in_k_number/SolutionTest.java
@@ -0,0 +1,26 @@
+package Programmers.kakao2022.find_decimals_in_k_number;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @Test
+    public void case1() {
+        int n = 437674, k = 3;
+        int expected = 3;
+        int actual = this.solution.solution(n, k);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void case2() {
+        int n = 110011, k = 10;
+        int expected = 2;
+        int actual = this.solution.solution(n, k);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
