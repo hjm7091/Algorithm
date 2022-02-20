3aa854aee0669e4a3c299b20de34d4d414088ab9
hjm7091 <hjm7091@naver.com>
2022-02-20 20:12:10 +0900

    코딩 인터뷰 완전 분석 (문제 2.1 연습)

---
+++ src/main/java/Study/Cracking_The_Coding_Interview_6E/LinkedList/Node.java
@@ -0,0 +1,78 @@
+package Study.Cracking_The_Coding_Interview_6E.LinkedList;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Node<T> {
+    private final T data;
+    private Node<T> next;
+    private Node<T> prev;
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public static <T> Node<T> initOneWay(Collection<T> list) {
+        if (list.size() == 0) return new Node<>(null);
+
+        Iterator<T> iterator = list.iterator();
+        Node<T> head = new Node<>(iterator.next());
+        Node<T> curr = head;
+        while (iterator.hasNext()){
+            curr.next = new Node<>(iterator.next());
+            curr = curr.next;
+        }
+
+        return head;
+    }
+
+    public boolean hasDuplicatedData() {
+        int size = 0;
+        Set<T> hashSet = new HashSet<>();
+        Node<T> p = this;
+
+        while (p != null) {
+            hashSet.add(p.data);
+            size++;
+            p = p.next;
+        }
+
+        return hashSet.size() != size;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
+
+    public Node<T> getPrev() {
+        return prev;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        Node<T> p = this;
+        while (p != null) {
+            sb.append(p.data);
+            if (p.next != null) sb.append(" => ");
+            p = p.next;
+        }
+
+        return sb.toString();
+    }
+}
