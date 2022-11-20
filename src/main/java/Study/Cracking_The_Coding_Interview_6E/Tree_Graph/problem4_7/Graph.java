16a9a6930096e90736f61c3940ea9c4be9888c24
hjm7091 <hjm7091@naver.com>
2022-11-20 20:08:03 +0900

    코딩 인터뷰 완전 분석 (문제 4.7 연습)

---
+++ src/main/java/Study/Cracking_The_Coding_Interview_6E/Tree_Graph/problem4_7/Graph.java
@@ -0,0 +1,31 @@
+package Study.Cracking_The_Coding_Interview_6E.Tree_Graph.problem4_7;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Graph {
+
+    private final List<Project> nodes = new ArrayList<>();
+    private final Map<String, Project> map = new HashMap<>();
+
+    public Project getOrCreateNode(String name) {
+        if (!map.containsKey(name)) {
+            Project node = new Project(name);
+            nodes.add(node);
+            map.put(name, node);
+        }
+        return map.get(name);
+    }
+
+    public void addEdge(String startName, String endName) {
+        Project start = getOrCreateNode(startName);
+        Project end = getOrCreateNode(endName);
+        start.addNeighbor(end);
+    }
+
+    public List<Project> getNodes() {
+        return nodes;
+    }
+}
